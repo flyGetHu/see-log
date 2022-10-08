ed90eda6eb697d618e82f22ed3f721879f9855e0
Huan <970780868@qq.com>
2022-10-08 16:37:27 +0800

    日志格式输出

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,17 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-use salvo::{prelude::*};
-
-#[handler]
-async fn hello_world(
-    _req: &mut Request,
-    _depot: &mut Depot,
-    res: &mut Response,
-    _ctrl: &mut FlowCtrl,
-) {
-    res.render("Hello world");
-}
+use salvo::prelude::*;
 
 #[handler]
 async fn see_log(req: &mut Request, res: &mut Response) {
-    let count = req.query("count").unwrap_or_else(|| {
-        1024
-    });
+    let count = req.query("count").unwrap_or_else(|| 1024);
     // let path = "C:\\Users\\97078\\Desktop\\fsdownload\\access.log";
     let path = "/home/work/express-app/express-app.log";
     let file = File::open(path).unwrap();
     let reader = BufReader::new(file);
     let lines = reader.lines();
-    let mut res_list = Vec::new();
+    let mut res_list = Vec::with_capacity(1024);
     for line in lines {
         let line_res = line.unwrap();
         res_list.push(line_res)
@@ -41,7 +29,7 @@ async fn see_log(req: &mut Request, res: &mut Response) {
 
 #[tokio::main]
 async fn main() {
-    let router = Router::new().get(hello_world)
+    let router = Router::new()
         .push(Router::with_path("/see/log").get(see_log));
     Server::new(TcpListener::bind("127.0.0.1:3000"))
         .serve(router)
