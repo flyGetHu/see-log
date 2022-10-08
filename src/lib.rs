87c3ec590829afa25b701db9c914fa313e04599e
Huan <970780868@qq.com>
2022-10-08 16:13:47 +0800

    日志查看

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+#[cfg(test)]
+mod tests {
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
+
+    #[test]
+    fn it_works() {
+        let path = "C:\\Users\\97078\\Desktop\\fsdownload\\access.log";
+        let file = File::open(path).unwrap();
+        let reader = BufReader::new(file);
+        let lines = reader.lines();
+        let mut res = Vec::new();
+        for line in lines {
+            res.push(line.unwrap())
+        }
+        let start = res.len() - 1024;
+        let res_final = &res[start..res.len()];
+        for line in res_final {
+            println!("{}",line)
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
-use salvo::{fs::NamedFile, prelude::*};
-use salvo::http::HeaderMap;
-use salvo::routing::get;
+use salvo::{prelude::*};
 
 #[handler]
 async fn hello_world(
@@ -16,13 +15,28 @@ async fn hello_world(
 
 #[handler]
 async fn see_log(req: &mut Request, res: &mut Response) {
-    let header = HeaderMap::new();
-    NamedFile::send_file(
-        "C:\\Users\\97078\\Desktop\\fsdownload\\access.log",
-        &header,
-        res,
-    )
-        .await;
+    let count = req.query("count").unwrap_or_else(|| {
+        1024
+    });
+    // let path = "C:\\Users\\97078\\Desktop\\fsdownload\\access.log";
+    let path = "/home/work/express-app/express-app.log";
+    let file = File::open(path).unwrap();
+    let reader = BufReader::new(file);
+    let lines = reader.lines();
+    let mut res_list = Vec::new();
+    for line in lines {
+        let line_res = line.unwrap();
+        res_list.push(line_res)
+    }
+    let start = res_list.len() - count;
+    let res_list_final = &res_list[start..res_list.len()];
+    let mut res_str = String::new();
+    let mut index = 1;
+    for line in res_list_final {
+        res_str += &format!("<p>{}. {}</p>\n", index, line);
+        index += 1;
+    }
+    res.render(Text::Html(format!("<html><body>{}</body></html>", res_str)))
 }
 
 #[tokio::main]
