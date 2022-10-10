136d366b91b40d02dd67adf795fdee10423a004d
Huan <970780868@qq.com>
2022-10-10 14:47:38 +0800

    服务503异常

--- src/handle/catcher_handle.rs
+++ src/handle/catcher_handle.rs
@@ -3,10 +3,15 @@ use salvo::prelude::StatusCode;
 
 pub struct Handle404;
 
+/// 处理http状态码异常情况
+/// 404 503
 impl Catcher for Handle404 {
     fn catch(&self, _req: &Request, _depot: &Depot, res: &mut Response) -> bool {
         if let Some(StatusCode::NOT_FOUND) = res.status_code() {
             res.render("Custom 404 Error Page");
+            true
+        } else if Some(StatusCode::SERVICE_UNAVAILABLE) = res.status_code() {
+            res.render("503 Service Unavailable");
             true
         } else {
             false
