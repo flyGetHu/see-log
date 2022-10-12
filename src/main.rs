238dff026a18eab34054b6189f6f4239c606eebe
Huan <970780868@qq.com>
2022-10-12 13:57:43 +0800

    启动日志打印

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
-use salvo::__private::tracing;
+use ferris_says::say;
 use salvo::prelude::*;
+use std::io::{stdout, BufWriter};
 
 use see_log::handle::*;
 use see_log::route::*;
 
 #[tokio::main]
 async fn main() {
+    //初始化日志记录
     tracing_subscriber::fmt().init();
-    //挂载路由
-    let service = Service::new(init_route()).with_catchers(inti_catcher());
+
     let address = "0.0.0.0:3000";
     let tcp_listener = TcpListener::bind(address);
-    tracing::info!("项目启动成功:{address}");
+
+    //打印启动日志
+    {
+        let message = format!("项目启动成功:{address}");
+        let stdout = stdout();
+        let mut writer = BufWriter::new(stdout.lock());
+        say(message.as_bytes(), message.chars().count() + 6, &mut writer).unwrap();
+    }
+
+    //初始化路由 启动webserver
+    let service = Service::new(init_route()).with_catchers(inti_catcher());
     Server::new(tcp_listener).serve(service).await;
 }
