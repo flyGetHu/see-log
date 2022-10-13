b9eeb5848ff34056e62580f39e027af53e549f26
Huan <970780868@qq.com>
2022-10-13 11:32:02 +0800

    读取日志异常情况处理

--- src/handle/catcher_handle.rs
+++ src/handle/catcher_handle.rs
@@ -1,4 +1,4 @@
-use salvo::prelude::StatusCode;
+use salvo::prelude::{StatusCode, StatusError};
 use salvo::{Catcher, Depot, Request, Response};
 
 pub struct CatcherHandle;
@@ -14,11 +14,11 @@ impl Catcher for CatcherHandle {
                 tracing::error!("客户端异常请求{:?}", req);
                 return match code {
                     StatusCode::NOT_FOUND => {
-                        res.render("Custom 404 Error Page");
+                        res.set_status_error(StatusError::not_found());
                         true
                     }
                     StatusCode::SERVICE_UNAVAILABLE => {
-                        res.render("503 Service Unavailable");
+                        res.set_status_error(StatusError::service_unavailable());
                         true
                     }
                     _status_code => false,
