03a4c0bbb182ac440a00f5bdafa593b60abf152a
huan <970780868@qq.com>
2023-03-17 17:57:18 +0800

    健康检测

--- src/route/mod.rs
+++ src/route/mod.rs
@@ -2,9 +2,11 @@ use salvo::logging::Logger;
 use salvo::Router;
 
 pub mod log_route;
+pub mod health_route;
 
 pub fn init_route() -> Router {
     Router::new()
         .hoop(Logger)
+        .push(Router::with_path("/health").get(health_route::health))
         .push(Router::with_path("/see/log").get(log_route::see_log))
 }
--- src/test/mod.rs
+++ src/test/mod.rs
@@ -1,6 +1,12 @@
+#[cfg(test)]
 mod closure_test;
+#[cfg(test)]
 mod file_test;
+#[cfg(test)]
 mod format_test;
+#[cfg(test)]
 mod match_test;
+#[cfg(test)]
 mod new_type_test;
+#[cfg(test)]
 mod num_test;
