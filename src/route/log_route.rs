8e98c83788f32d5d05f5fa38e800b83ad953bd8b
huan <970780868@qq.com>
2023-03-25 19:17:35 +0800

    处理异常

--- src/route/log_route.rs
+++ src/route/log_route.rs
@@ -10,16 +10,15 @@ const MAX_LINE_SIZE: usize = 1024;
 pub async fn see_log(req: &mut Request, res: &mut Response) {
     let count = req.query("count").unwrap_or(MAX_LINE_SIZE / 2).min(MAX_LINE_SIZE);
     let file_path_query = req.query("file_path");
-    if file_path_query.is_none() {
-        res.set_status_error(StatusError::internal_server_error().with_detail("file_path必传"));
-        return;
-    }
-    let file_path = file_path_query.unwrap();
-    let result = entity::log_file::LogFile { file_path, count }.load_log_file();
-    match result {
-        Ok(data) => res.render(format!("{}", data)),
-        Err(err) => {
-            res.set_status_error(StatusError::internal_server_error().with_detail(err))
+    if let Some(file_path) = file_path_query {
+        let result = entity::log_file::LogFile { file_path, count }.load_log_file();
+        match result {
+            Ok(data) => res.render(format!("{}", data)),
+            Err(err) => {
+                res.set_status_error(StatusError::internal_server_error().with_detail(err))
+            }
         }
+    } else {
+        res.set_status_error(StatusError::internal_server_error().with_detail("file_path必传"));
     }
 }
