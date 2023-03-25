737898ce191f6237638959a7a74b33f338e8a931
huan <970780868@qq.com>
2023-03-25 19:14:05 +0800

    打印优化

--- src/route/log_route.rs
+++ src/route/log_route.rs
@@ -8,23 +8,18 @@ const MAX_LINE_SIZE: usize = 1024;
 
 #[handler]
 pub async fn see_log(req: &mut Request, res: &mut Response) {
-    let mut count = req.query("count").unwrap_or_else(|| MAX_LINE_SIZE / 2);
-    if count > MAX_LINE_SIZE {
-        count = MAX_LINE_SIZE
-    }
+    let count = req.query("count").unwrap_or(MAX_LINE_SIZE / 2).min(MAX_LINE_SIZE);
     let file_path_query = req.query("file_path");
-    match file_path_query {
-        None => {
-            res.set_status_error(StatusError::internal_server_error().with_detail("file_path必传"));
-        }
-        Some(file_path) => {
-            let result = entity::log_file::LogFile { file_path, count }.load_log_file();
-            match result {
-                Ok(data) => res.render(format!("{}", data)),
-                Err(err) => {
-                    res.set_status_error(StatusError::internal_server_error().with_detail(err))
-                }
-            }
+    if file_path_query.is_none() {
+        res.set_status_error(StatusError::internal_server_error().with_detail("file_path必传"));
+        return;
+    }
+    let file_path = file_path_query.unwrap();
+    let result = entity::log_file::LogFile { file_path, count }.load_log_file();
+    match result {
+        Ok(data) => res.render(format!("{}", data)),
+        Err(err) => {
+            res.set_status_error(StatusError::internal_server_error().with_detail(err))
         }
     }
 }
