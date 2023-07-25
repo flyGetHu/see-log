efb6bd0ddb12985f4b07d6145b49c224434bab17
huan <970780868@qq.com>
2023-07-25 23:32:57 +0800

    Merge branch 'master' of https://github.com/flyGetHu/see-log

--- src/utils/file_util.rs
+++ src/utils/file_util.rs
@@ -1,23 +1,59 @@
 use std::collections::VecDeque;
 use std::fs::File;
-use std::io::{self, BufRead, BufReader};
+use std::io::{self, BufRead, BufReader, Seek};
 use std::path::Path;
 
-/// Read the last few lines of a file
-///
-/// # Arguments
-///
-/// * `file_path` - File path
-/// * `max_res_count` - Maximum number of lines to read
-///
-/// # Returns
-///
-/// A `Result` containing a `Vec` of `String`s representing the last few lines of the file.
-///
-/// # Errors
-///
-/// Returns an `io::Error` if the file cannot be opened or read.
+/// Reads the last `max_res_count` lines of a file at `file_path`.
+/// If the file size is less than or equal to 10MB, it reads the entire file.
+/// Returns a `VecDeque` containing the lines read, with a maximum capacity of `max_res_count`.
 pub fn read_file_tail(
+    file_path: impl AsRef<Path>,
+    max_res_count: usize,
+) -> io::Result<VecDeque<String>> {
+    let max_size = 1024;
+    let metadata = match std::fs::metadata(&file_path) {
+        Ok(metadata) => metadata,
+        Err(err) => return Err(err),
+    };
+    let file_size = metadata.len();
+    if file_size <= max_size {
+        match read_file_tail_all(file_path, max_res_count) {
+            Ok(data) => return Ok(data),
+            Err(err) => return Err(err),
+        }
+    }
+    let file = match File::open(&file_path) {
+        Ok(file) => file,
+        Err(err) => return Err(err),
+    };
+    let mut reader = BufReader::new(file);
+    let mut res_vec = VecDeque::with_capacity(max_res_count);
+    let mut pos = file_size - max_size;
+    let mut buf = String::new();
+    loop {
+        match reader.seek(std::io::SeekFrom::Start(pos)) {
+            Ok(_) => {}
+            Err(err) => return Err(err),
+        }
+        match reader.read_line(&mut buf) {
+            Ok(0) => break,
+            Ok(_) => {
+                if res_vec.len() >= max_res_count {
+                    res_vec.pop_front();
+                }
+                res_vec.push_back(buf.clone());
+                buf.clear();
+            }
+            Err(err) => return Err(err),
+        }
+        pos += buf.len() as u64;
+    }
+    Ok(res_vec)
+}
+
+/// Reads all lines of a file at `file_path`.
+/// Returns a `VecDeque` containing the lines read, with a maximum capacity of `max_res_count`.
+pub fn read_file_tail_all(
     file_path: impl AsRef<Path>,
     max_res_count: usize,
 ) -> io::Result<VecDeque<String>> {
