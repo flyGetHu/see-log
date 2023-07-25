0ce152c9e9a12a9952d54483e40b67add295cfee
huan <970780868@qq.com>
2023-07-25 23:49:48 +0800

    读取文件超过指定大小使用seek读取

--- src/utils/file_util.rs
+++ src/utils/file_util.rs
@@ -3,24 +3,26 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader, Seek};
 use std::path::Path;
 
-/// Reads the last `max_res_count` lines of a file at `file_path`.
-/// If the file size is less than or equal to 10MB, it reads the entire file.
-/// Returns a `VecDeque` containing the lines read, with a maximum capacity of `max_res_count`.
+// 读取文件超过指定大小时，使用seek方式读取
+const MAX_LINE_LENGTH: u64 = 1024 * 1024 * 10;
+
+/// Reads the last `max_res_count` lines of the file.
+/// If the file size is less than or equal to max_size, it reads the entire file.
 pub fn read_file_tail(
     file_path: impl AsRef<Path>,
     max_res_count: usize,
 ) -> io::Result<VecDeque<String>> {
-    let max_size = 1024;
     let metadata = match std::fs::metadata(&file_path) {
         Ok(metadata) => metadata,
         Err(err) => return Err(err),
     };
     let file_size = metadata.len();
-    if file_size <= max_size {
-        match read_file_tail_all(file_path, max_res_count) {
-            Ok(data) => return Ok(data),
-            Err(err) => return Err(err),
-        }
+    // If the file size is less than or equal to max_size, it reads the entire file.
+    if file_size <= MAX_LINE_LENGTH {
+        return match read_file_tail_all(file_path, max_res_count) {
+            Ok(data) => Ok(data),
+            Err(err) => Err(err),
+        };
     }
     let file = match File::open(&file_path) {
         Ok(file) => file,
@@ -28,7 +30,8 @@ pub fn read_file_tail(
     };
     let mut reader = BufReader::new(file);
     let mut res_vec = VecDeque::with_capacity(max_res_count);
-    let mut pos = file_size - max_size;
+    // Start reading from the end of the file. 只读取最后10MB的内容
+    let mut pos = file_size - MAX_LINE_LENGTH;
     let mut buf = String::new();
     loop {
         match reader.seek(std::io::SeekFrom::Start(pos)) {
@@ -42,17 +45,17 @@ pub fn read_file_tail(
                     res_vec.pop_front();
                 }
                 res_vec.push_back(buf.clone());
+                // `buf` is now empty, so we can reuse it.
+                pos += buf.len() as u64;
                 buf.clear();
             }
             Err(err) => return Err(err),
         }
-        pos += buf.len() as u64;
     }
     Ok(res_vec)
 }
 
-/// Reads all lines of a file at `file_path`.
-/// Returns a `VecDeque` containing the lines read, with a maximum capacity of `max_res_count`.
+/// Reads the entire file.
 pub fn read_file_tail_all(
     file_path: impl AsRef<Path>,
     max_res_count: usize,
